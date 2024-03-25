99f8db3c04cfa7011d56614fa912fb4fe5d16ee6
Vladimir988 <optimist0706@gmail.com>
2024-03-25 12:57:59 +0200

    User List

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import {useTypedSelector} from "../hooks/useTypedSelector";
+import React, {useEffect} from 'react';
+import {useAppDispatch, useTypedSelector} from "../hooks/useTypedSelector";
+import {fetchUsers} from "../store/action-creators/user";
+import {useActions} from "../hooks/useActions";
 
 const UserList: React.FC = () => {
     const {users, loading, error} = useTypedSelector(state => state.user);
+    // const dispatch = useAppDispatch();
+    const {fetchUsers} = useActions();
+
+    useEffect(() => {
+        // dispatch(fetchUsers());
+        fetchUsers();
+    }, []);
+
+    if(loading) {
+        return <h1>Loading...</h1>;
+    }
+
+    if(error) {
+        return <h1>{error}</h1>;
+    }
 
     return (
         <div>
-            
+            {users.map(user =>
+                <div key={user.id}>{user.name}</div>
+            )}
         </div>
     );
 };
--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,4 +1,6 @@
-import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store/reducers";
+import {AppDispatch} from "../store";
 
 export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch: () => AppDispatch = useDispatch;
